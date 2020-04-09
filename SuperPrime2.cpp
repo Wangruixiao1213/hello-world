05aa52f8176e33eefa388f5ae536378e9e529cb9
wangruixiao <847573508@qq.com>
2020-04-09 18:19:44 +0800

    superprime.cpp finished

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,70 +1,102 @@
-//作业：完成以下框架的代码细节，程序能编译运行得到正确结果 
+
 #include <iostream>
 class SuperPrime {
-  public:
-  	SuperPrime():number(0) {
-	}
-  	~SuperPrime() {
-	}
-  	bool isSuperPrime() {
-  	  split();
-  	  int a = sum();
-	  int b = multi();
-	  int c = squareSum();
-	  if (isPrime(number) && isPrime(a) && isPrime(b) && isPrime(c))
-	    return true; 
-  	  return false;
-	}
-  private:
-  	const int number;
-  	int N[100], size;
-  	bool isPrime(int n) { 
-  	
-  	  return false;
-	}
-	void split() {
-	  // number split into N
-	}
-	int sum() {
-	  return 0;
-	}
-	int multi() {
-	  return 0;
-	}
-	int squareSum() {
-	  return 0;
-	}
+public:
+    int number;
+    void _number(int n) {
+        number = n;
+    }
+    bool isSuperPrime() {
+        split();
+        int a = sum();
+        int b = multi();
+        int c = squareSum();
+        if (isPrime(number) && isPrime(a) && isPrime(b) && isPrime(c))
+            return true;
+        return false;
+    }
+
+private:
+    int N[20], size_N;
+    bool isPrime(int x) {
+        int i = 2;
+        if(x == 0 || x == 1) return false;
+        for(i = 2; i < x; i++){
+            if(x % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    void split() {
+        int _number = number,i;
+        for(i = 0; _number > 0; i++){
+            N[i] = _number % 10;
+            _number /= 10;
+        }
+        size_N = i;
+    }
+    int sum() {
+        int sum_number = 0;
+        for (int i=0;i<size_N;i++){
+            sum_number+= N[i];
+        }
+        return sum_number;
+    }
+    int multi() {
+        int multi_num = 1;
+        int i;
+        for(i = 0; i<size_N; i++)
+            multi_num *= N[i];
+        return multi_num;
+    }
+    int squareSum() {
+        int square = 0;
+        int i;
+        for(i = 0; i < size_N; i++)
+            square += (N[i] * N[i]);
+        return square;
+    }
 };
 class Set {
-  public:
-  	Set(int from, int to) {
-  	  size = 0;
-	}
-  	~Set() {
-	}
-  	int count() {
-  	  int count = 0;
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i].isSuperPrime())
-  	      count += 1;
-	  return count; 
-	}
-  	int sum() {
-  	  int sum = 0;
-  	  /*
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i].isSuperPrime())
-  	      sum += set[i];
-  	      */ 
-	  return sum; 
-	}
-  private:
-  	SuperPrime set[1000];
-  	int size;
+public:
+    Set(int from, int to) {
+        size = to - from;
+        _from = from;
+        _to = to;
+    }
+    ~Set() {
+    }
+    int count() {
+        int count = 0;
+        int i;
+        for (i = 0; i < size; i++) {
+            set[i]._number(i + _from);
+            if(set[i].isSuperPrime()) count += 1;
+        }
+        return count;
+    }
+    int sum() {
+        int sum = 0;
+        int i;
+        for (i = 0; i < size; i++) {
+            set[i]._number(i + _from);
+            if(set[i].isSuperPrime()){
+                sum += set[i].number;
+                //std::cout << set[i].number << std::endl;
+            }
+
+        }
+        return sum;
+    }
+private:
+    SuperPrime set[1000];
+    int size;
+    int _from, _to;
 };
 int main() {
-  Set ss(100, 999);
-  std::cout << "How Many: " << ss.count() << std::endl;
-  std::cout << "Sum is " << ss.sum() << std::endl;
-  return 0;
+    Set ss(100, 999);
+    std::cout << "How Many: " << ss.count() << std::endl;
+    std::cout << "Sum is " << ss.sum() << std::endl;
+    return 0;
 }
